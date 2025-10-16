4d2832a430ac445ba7c6d42cb5d51338d2ef65eb
EmoSavage16 <emirhanfiliz703@gmail.com>
2025-10-17 02:28:39 +0300

    karekter-yürüme-
    animasyonlar eklencek

---
+++ Assets/karekter/Scripts/KarakterKontrol .cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KarakterKontrol : MonoBehaviour
+{
+    private Animator anim;
+    private float karakterHiz = 5f;
+
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Hareket();
+    }
+
+    void Hareket()
+    {
+        float yatay = Input.GetAxis("Horizontal");
+        float dikey = Input.GetAxis("Vertical");
+
+        anim.SetFloat("Horizontal", yatay);
+        anim.SetFloat("Vertical", dikey);
+
+        // Karakterin hareket yönü
+        Vector3 hareket = new Vector3(yatay, 0, dikey);
+
+        // Normalize edip hızla çarpıyoruz
+        this.gameObject.transform.Translate(hareket.normalized * karakterHiz * Time.deltaTime, Space.World);
+
+        // Karakter dönsün (eğer hareket ediyorsa)
+        if (hareket != Vector3.zero)
+        {
+            transform.forward = hareket;
+        }
+    }
+}
