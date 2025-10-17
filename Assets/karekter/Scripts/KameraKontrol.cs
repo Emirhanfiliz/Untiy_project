aefab815caa6e3d45c6223e7bac19a71ecc2db27
EmoSavage16 <emirhanfiliz703@gmail.com>
2025-10-17 17:24:00 +0300

    kamera tamam

--- Assets/karekter/Scripts/KameraKontrol.cs
+++ Assets/karekter/Scripts/KameraKontrol.cs
@@ -4,14 +4,19 @@ public class KameraKontrol : MonoBehaviour
 {
     public Transform hedef;
     public Vector3 hedefMesafe;
-    public float FareHassasiyeti;
+    [SerializeField]
+    private float FareHassasiyeti = 2f;
     float fareX, fareY;
+
+    Vector3 objRot;
+    public Transform KarakterVucut;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-
+        Cursor.visible = false;
     }
+
     void Update()
     {
 
@@ -19,11 +24,30 @@ public class KameraKontrol : MonoBehaviour
 
     void LateUpdate()
     {
-        this.transform.position = Vector3.Lerp(this.transform.position, hedef.position + hedefMesafe, Time.deltaTime * 10);
-        fareX += Input.GetAxis("Mouse X");
-        fareY += Input.GetAxis("Mouse Y");
-        this.transform.eulerAngles = new Vector3(fareY, fareX, 0);
-        hedef.transform.eulerAngles = new Vector3(0, fareX, 0);
+        if (hedef == null) return;
+
+
+        transform.position = Vector3.Lerp(transform.position, hedef.position + hedefMesafe, Time.deltaTime * 10f);
+
+
+        fareX += Input.GetAxis("Mouse X") * FareHassasiyeti;
+        fareY -= Input.GetAxis("Mouse Y") * FareHassasiyeti;
+
+
+        fareY = Mathf.Clamp(fareY, -40f, 25f);
+
+
+        transform.rotation = Quaternion.Euler(fareY, fareX, 0f);
+        hedef.rotation = Quaternion.Euler(0f, fareX, 0f);
+
+
+        if (KarakterVucut != null)
+        {
+            float bodyTiltX = fareY + 10f;
+
+            bodyTiltX = Mathf.Clamp(bodyTiltX, -40f, 45f);
+            KarakterVucut.rotation = Quaternion.Euler(bodyTiltX, fareX, 0f);
+        }
     }
 
 }
