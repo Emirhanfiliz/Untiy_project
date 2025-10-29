2bd4c20ce134e2b436bfaeff18bad17bcdd6be29
bicerr <mehmetbe867@gmail.com>
2025-10-29 20:07:50 +0300

    ateş efekti ve reload
    .

--- Assets/karekter/Scripts/Fire.cs
+++ Assets/karekter/Scripts/Fire.cs
@@ -9,6 +9,10 @@ public class Fire : MonoBehaviour
     KarakterKontrol hpKontrol;
     Animator anim;
 
+    private float sarjor = 5;
+    private float cephane = 10;
+    private float sarjorKapasitesi = 5;
+
     void Start()
     {
         kamera = Camera.main;
@@ -23,37 +27,62 @@ public class Fire : MonoBehaviour
         {
             if (Input.GetMouseButton(0))
             {
-                anim.SetBool("atesEt", true);
+                if (sarjor > 0)
+                {
+                    anim.SetBool("atesEt", true);
+                }
+                if (sarjor <= 0)
+                {
+                    anim.SetBool("atesEt", false);
+                }
+                if (sarjor <= 0 && cephane > 0)
+                {
+                    anim.SetBool("sarjorDegistirme", true);
+                    cephane -= sarjorKapasitesi - sarjor;
+                    sarjor = sarjorKapasitesi;
+                }
             }
             else
             {
                 anim.SetBool("atesEt", false);
             }
         }
-        else
-        {
-            anim.SetBool("atesEt", false);
-        }
+       
     }
+    public void SarjorDegistirme()
+     
+        {
+            cephane -= sarjorKapasitesi - sarjor;
+            sarjor = sarjorKapasitesi;
+            anim.SetBool("sarjorDegistirme", false);
+
+        }
+    
 
     public void AtesEtme()
     {
         if (kamera == null) kamera = Camera.main;
         if (kamera == null) return;
 
-        muzzle.Play();
-
-        Ray ray = kamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, menzil, Ecikatman.value))
+        if (sarjor > 0)
         {
-            Dusman dusman = hit.collider.GetComponent<Dusman>();
-            if (dusman == null)
+
+
+            sarjor--;
+            muzzle.Play();
+
+            Ray ray = kamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, menzil, Ecikatman.value))
             {
-                return;
+                Dusman dusman = hit.collider.GetComponent<Dusman>();
+                if (dusman == null)
+                {
+                    return;
+                }
+                dusman.HasarAl();
             }
-            dusman.HasarAl();
         }
     }
 }
